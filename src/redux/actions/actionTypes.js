7f004ff1db7358f372aa9485c43fbc48011f03e7
angeldvilla <escarlata.2003@gmail.com>
2023-10-04 00:35:10 -0500

    Configure redux

---
+++ src/redux/actions/actionTypes.js
@@ -0,0 +1,6 @@
+/* export const ACTION_1 = "ACTION_1";
+export const ACTION_2 = "ACTION_2";
+export const ACTION_3 = "ACTION_3";
+export const ACTION_4 = "ACTION_4";
+export const ACTION_5 = "ACTION_5";
+export const ACTION_6 = "ACTION_6"; */
---
+++ src/redux/store/store.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers/reducer';
+/* ------------------------------------------------------------- */ 
+
+// Esta línea es para conectar con la extensión del navegador => REDUX DEVTOOLS 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    rootReducer,
+    composeEnhancer(applyMiddleware(thunk)) // Esta línea es para poder hacer peticiones a un server
+);
+/* ------------------------------------------------------------- */ 
+
+export default store;
