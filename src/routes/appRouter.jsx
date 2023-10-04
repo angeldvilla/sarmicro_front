e915cc8b81a5c02296f4c646e37f82e6b6f48bf2
angeldvilla <escarlata.2003@gmail.com>
2023-10-04 00:29:26 -0500

    App Privates Router

--- src/routes/appRouter.jsx
+++ src/routes/appRouter.jsx
@@ -2,17 +2,27 @@ import React from "react";
 import { Routes, Route } from "react-router";
 
 /* VIEWS */
+import Home from "../views/Home/Home.jsx";
 import Login from "../views/Login/Login.jsx";
 import Register from "../views/Register/Register.jsx";
 
 const RoutesApp = () => {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/registrarse" element={<Register />} />     
-      
+      <Route index element={<Login />} />
+      <Route path="/registrarse" element={<Register />} />
+
+      {/* RUTAS PROTEGIDAS */}
+      <Route>
+        <Route path="/home/*" element={<Home />} />
+       {/*  <Route path="pago_polizas" element={<Payments />} />
+        <Route path="valor_poliza" element={<Policy />} />
+        <Route path="vehiculos" element={<Vehicles />} /> */}
+      </Route>
+
+
     </Routes>
   );
-}
+};
 
 export default RoutesApp;
