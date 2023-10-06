56ba45b8cad4d97af8c6c5e925ea2b50d67ceb1d
angeldvilla <escarlata.2003@gmail.com>
2023-10-05 23:15:13 -0500

    Fix datatables

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,9 @@ module.exports = {
     content: [
       "./index.html",
       "./src/**/*.{html,js,jsx,tsx}",
-      'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
     ],
     theme: {
       extend: {},
     },
-    plugins: [
-      require('flowbite/plugin')
-    ],
+    plugins: [],
   }
