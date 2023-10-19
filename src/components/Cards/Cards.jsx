cda5bc4146c9dbbde86a558afaa01d79cbd9b2f1
angeldvilla <escarlata.2003@gmail.com>
2023-10-19 14:30:49 -0500

    Fix changes

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -5,12 +5,12 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
-import PaidIcon from "@mui/icons-material/Paid";
+/* import PaidIcon from "@mui/icons-material/Paid";
 import PolicyIcon from "@mui/icons-material/Policy";
-import RequestQuoteIcon from '@mui/icons-material/RequestQuote';
+import RequestQuoteIcon from "@mui/icons-material/RequestQuote";
 import LocalTaxiIcon from "@mui/icons-material/LocalTaxi";
 import SavingsIcon from "@mui/icons-material/Savings";
-import RestoreFromTrashIcon from '@mui/icons-material/RestoreFromTrash';
+import RestoreFromTrashIcon from "@mui/icons-material/RestoreFromTrash"; */
 import styles from "./card.module.css";
 import valorPoliza from "../../assets/images/valorPolizas.jpg";
 import pagoPolizas from "../../assets/images/pagoPolizas.jpg";
@@ -22,44 +22,44 @@ import deleted from "../../assets/images/deleted.png";
 export const CardPolicy = () => {
   return (
     <div className={styles.cardContainer}>
-      <Card style={{ borderRadius: "1.2em" }}>
-        <CardActionArea>
-          <CardMedia
-            className={styles.cardImage}
-            component="img"
-            height="140"
-            image={valorPoliza}
-            /* https://img.freepik.com/vector-gratis/iconos-polizas-seguros_603843-478.jpg?w=2000 */
-            alt="Valor de Polizas"
-          />
-          <CardContent>
-            <Typography
-              gutterBottom
-              variant="h5"
-              component="div"
-              className={styles.cardText}
-            >
-              Valor de Polizas
-            </Typography>
-            <Typography
-              variant="body2"
-              color="text.secondary"
-              className={styles.cardText}
-            >
-              Esta tarjeta muestra información relacionada con los valores de
-              las polizas.
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-      <div className={styles.overlay}>
-        <NavLink to="/valor-poliza">
-          <Typography className={styles.overlayButton}>
+      <NavLink to="/valor-poliza">
+        <Card style={{ borderRadius: "1.2em" }}>
+          <CardActionArea>
+            <CardMedia
+              className={styles.cardImage}
+              component="img"
+              height="140"
+              image={valorPoliza}
+              /* https://img.freepik.com/vector-gratis/iconos-polizas-seguros_603843-478.jpg?w=2000 */
+              alt="Valor de Polizas"
+            />
+            <CardContent>
+              <Typography
+                gutterBottom
+                variant="h5"
+                component="div"
+                className={styles.cardText}
+              >
+                Valor de Polizas
+              </Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                className={styles.cardText}
+              >
+                Esta tarjeta muestra información relacionada con los valores de
+                las polizas.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <div className={styles.overlay}>
+          {/* <Typography className={styles.overlayButton}>
             Ver Detalles
             <PolicyIcon style={{ marginLeft: 10, fontSize: "large" }} />
-          </Typography>
-        </NavLink>
-      </div>
+          </Typography> */}
+        </div>
+      </NavLink>
     </div>
   );
 };
@@ -67,46 +67,46 @@ export const CardPolicy = () => {
 export const CardPayments = () => {
   return (
     <div className={styles.cardContainer}>
-      <Card style={{ borderRadius: "1.2em" }}>
-        <CardActionArea>
-          <CardMedia
-            className={styles.cardImage}
-            component="img"
-            height="140"
-            image={pagoPolizas}
-            /*https://img.freepik.com/vector-premium/concepto-ganancia-billetera-dinero-pago-linea-ilustracion-vectorial-plana-banner-pagina-destino_128772-915.jpg
+      <NavLink to="/pago-polizas">
+        <Card style={{ borderRadius: "1.2em" }}>
+          <CardActionArea>
+            <CardMedia
+              className={styles.cardImage}
+              component="img"
+              height="140"
+              image={pagoPolizas}
+              /*https://img.freepik.com/vector-premium/concepto-ganancia-billetera-dinero-pago-linea-ilustracion-vectorial-plana-banner-pagina-destino_128772-915.jpg
            
           https://www.autopista.es/uploads/s1/57/92/67/2/5eec6d370de69406553493f3-que-seguro-de-coche-elegir-tipos-de-polizas-ventajas-e-inconvenientes.jpeg */
-            alt="Pago de Polizas"
-          />
-          <CardContent>
-            <Typography
-              gutterBottom
-              variant="h5"
-              component="div"
-              className={styles.cardText}
-            >
-              Pago de Polizas
-            </Typography>
-            <Typography
-              variant="body2"
-              color="text.secondary"
-              className={styles.cardText}
-            >
-              Esta tarjeta muestra información relacionada con los respectivos
-              pagos de las polizas.
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-      <div className={styles.overlay}>
-        <NavLink to="/pago-polizas">
-          <Typography className={styles.overlayButton}>
+              alt="Pago de Polizas"
+            />
+            <CardContent>
+              <Typography
+                gutterBottom
+                variant="h5"
+                component="div"
+                className={styles.cardText}
+              >
+                Pago de Polizas
+              </Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                className={styles.cardText}
+              >
+                Esta tarjeta muestra información relacionada con los respectivos
+                pagos de las polizas.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <div className={styles.overlay}>
+          {/* <Typography className={styles.overlayButton}>
             Ver Detalles
             <PaidIcon style={{ marginLeft: 10, fontSize: "large" }} />
-          </Typography>
-        </NavLink>
-      </div>
+          </Typography>*/}
+        </div>
+      </NavLink>
     </div>
   );
 };
@@ -114,44 +114,44 @@ export const CardPayments = () => {
 export const CardCuotas = () => {
   return (
     <div className={styles.cardContainer}>
-      <Card style={{ borderRadius: "1.2em" }}>
-        <CardActionArea>
-          <CardMedia
-            className={styles.cardImage}
-            component="img"
-            height="140"
-            image={cuotasPolizas}
-            /* https://img.freepik.com/vector-gratis/iconos-polizas-seguros_603843-478.jpg?w=2000 */
-            alt="Cuotas"
-          />
-          <CardContent>
-            <Typography
-              gutterBottom
-              variant="h5"
-              component="div"
-              className={styles.cardText}
-            >
-              Cuotas
-            </Typography>
-            <Typography
-              variant="body2"
-              color="text.secondary"
-              className={styles.cardText}
-            >
-              Esta tarjeta muestra información relacionada con las cuotas
-              registradas de las polizas.
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-      <div className={styles.overlay}>
-        <NavLink to="/cuotas">
-          <Typography className={styles.overlayButton}>
+      <NavLink to="/cuotas">
+        <Card style={{ borderRadius: "1.2em" }}>
+          <CardActionArea>
+            <CardMedia
+              className={styles.cardImage}
+              component="img"
+              height="140"
+              image={cuotasPolizas}
+              /* https://img.freepik.com/vector-gratis/iconos-polizas-seguros_603843-478.jpg?w=2000 */
+              alt="Cuotas"
+            />
+            <CardContent>
+              <Typography
+                gutterBottom
+                variant="h5"
+                component="div"
+                className={styles.cardText}
+              >
+                Cuotas
+              </Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                className={styles.cardText}
+              >
+                Esta tarjeta muestra información relacionada con las cuotas
+                registradas de las polizas.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <div className={styles.overlay}>
+          {/*  <Typography className={styles.overlayButton}>
             Ver Detalles
             <RequestQuoteIcon style={{ marginLeft: 10, fontSize: "large" }} />
-          </Typography>
-        </NavLink>
-      </div>
+          </Typography>*/}
+        </div>
+      </NavLink>
     </div>
   );
 };
@@ -159,45 +159,45 @@ export const CardCuotas = () => {
 export const CardVehicles = () => {
   return (
     <div className={styles.cardContainer}>
-      <Card style={{ borderRadius: "1.2em" }}>
-        <CardActionArea>
-          <CardMedia
-            className={styles.cardImage}
-            component="img"
-            height="140"
-            image={parqueAutomotor}
-            /* https://img.freepik.com/vector-premium/diseno-zona-aparcamiento-o-parque_24877-34208.jpg
+      <NavLink to="/vehiculos">
+        <Card style={{ borderRadius: "1.2em" }}>
+          <CardActionArea>
+            <CardMedia
+              className={styles.cardImage}
+              component="img"
+              height="140"
+              image={parqueAutomotor}
+              /* https://img.freepik.com/vector-premium/diseno-zona-aparcamiento-o-parque_24877-34208.jpg
            https://img.freepik.com/vector-gratis/estacionamiento-isometrico-coloreado_1284-25255.jpg?w=2000 */
-            alt="Parque Automotor"
-          />
-          <CardContent>
-            <Typography
-              gutterBottom
-              variant="h5"
-              component="div"
-              className={styles.cardText}
-            >
-              Parque Automotor
-            </Typography>
-            <Typography
-              variant="body2"
-              color="text.secondary"
-              className={styles.cardText}
-            >
-              Esta tarjeta muestra información relacionada con el parque
-              automotor y los vehiculos.
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-      <div className={styles.overlay}>
-        <NavLink to="/vehiculos">
-          <Typography className={styles.overlayButton}>
+              alt="Parque Automotor"
+            />
+            <CardContent>
+              <Typography
+                gutterBottom
+                variant="h5"
+                component="div"
+                className={styles.cardText}
+              >
+                Parque Automotor
+              </Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                className={styles.cardText}
+              >
+                Esta tarjeta muestra información relacionada con el parque
+                automotor y los vehiculos.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <div className={styles.overlay}>
+          {/*  <Typography className={styles.overlayButton}>
             Ver Detalles
             <LocalTaxiIcon style={{ marginLeft: 10, fontSize: "large" }} />
-          </Typography>
-        </NavLink>
-      </div>
+          </Typography> */}
+        </div>
+      </NavLink>
     </div>
   );
 };
@@ -205,87 +205,89 @@ export const CardVehicles = () => {
 export const CashBox = () => {
   return (
     <div className={styles.cardContainer}>
-      <Card style={{ borderRadius: "1.2em" }}>
-        <CardActionArea>
-          <CardMedia
-            className={styles.cardImage}
-            component="img"
-            height="140"
-            image={cuadreCaja}
-            alt="Cuadre de Caja"
-          />
-          <CardContent>
-            <Typography
-              gutterBottom
-              variant="h5"
-              component="div"
-              className={styles.cardText}
-            >
-              Cuadre de Caja
-            </Typography>
-            <Typography
-              variant="body2"
-              color="text.secondary"
-              className={styles.cardText}
-            >
-              Esta tarjeta muestra información relacionada con el cuadre de
-              caja.
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-      <div className={styles.overlay}>
-        <a href="/caja">
-          <Typography className={styles.overlayButton}>
+      <a href="/caja">
+        <Card style={{ borderRadius: "1.2em" }}>
+          <CardActionArea>
+            <CardMedia
+              className={styles.cardImage}
+              component="img"
+              height="140"
+              image={cuadreCaja}
+              alt="Cuadre de Caja"
+            />
+            <CardContent>
+              <Typography
+                gutterBottom
+                variant="h5"
+                component="div"
+                className={styles.cardText}
+              >
+                Cuadre de Caja
+              </Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                className={styles.cardText}
+              >
+                Esta tarjeta muestra información relacionada con el cuadre de
+                caja.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <div className={styles.overlay}>
+          {/*<Typography className={styles.overlayButton}>
             Ver Detalles
             <SavingsIcon style={{ marginLeft: 10, fontSize: "large" }} />
-          </Typography>
-        </a>
-      </div>
+          </Typography>*/}
+        </div>
+      </a>
     </div>
   );
 };
 
-
 export const DetailPolicy = () => {
   return (
     <div className={styles.cardContainer}>
-      <Card style={{ borderRadius: "1.2em" }}>
-        <CardActionArea>
-          <CardMedia
-            className={styles.cardImage}
-            component="img"
-            height="140"
-            image={deleted}
-            alt="Detalle Polizas"
-          />
-          <CardContent>
-            <Typography
-              gutterBottom
-              variant="h5"
-              component="div"
-              className={styles.cardText}
-            >
-              Detalle Polizas
-            </Typography>
-            <Typography
-              variant="body2"
-              color="text.secondary"
-              className={styles.cardText}
-            >
-              Esta tarjeta muestra información de la eliminación en cascada de las polizas
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-      <div className={styles.overlay}>
-        <a href="/detalle-polizas">
-          <Typography className={styles.overlayButton}>
+      <a href="/detalle-polizas">
+        <Card style={{ borderRadius: "1.2em" }}>
+          <CardActionArea>
+            <CardMedia
+              className={styles.cardImage}
+              component="img"
+              height="140"
+              image={deleted}
+              alt="Detalle Polizas"
+            />
+            <CardContent>
+              <Typography
+                gutterBottom
+                variant="h5"
+                component="div"
+                className={styles.cardText}
+              >
+                Detalle Polizas
+              </Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                className={styles.cardText}
+              >
+                Esta tarjeta muestra información de la eliminación en cascada de
+                las polizas
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <div className={styles.overlay}>
+          {/*<Typography className={styles.overlayButton}>
             Ver Detalles
-            <RestoreFromTrashIcon style={{ marginLeft: 10, fontSize: "large" }} />
-          </Typography>
-        </a>
-      </div>
+            <RestoreFromTrashIcon
+              style={{ marginLeft: 10, fontSize: "large" }}
+            />
+          </Typography>*/}
+        </div>
+      </a>
     </div>
   );
 };
